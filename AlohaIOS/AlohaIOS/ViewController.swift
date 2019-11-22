62742dda215acd934590cd29ea2b8a3cca2c6281
Luiz Henrique Carminati Campos <luizhcarminati@gmail.com>
2019-11-22 15:41:26 -0300

    first commit

---
+++ AlohaIOS/AlohaIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlohaIOS
+//
+//  Created by zupper on 22/11/19.
+//  Copyright © 2019 Luizhcarminati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
