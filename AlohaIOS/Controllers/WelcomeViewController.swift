ef47001139bb5d58926fd5e65920efe7c278a06f
Luiz Henrique Carminati Campos <luizhcarminati@gmail.com>
2019-12-02 09:47:16 -0300

    Layout finalizado

---
+++ AlohaIOS/Controllers/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  AlohaIOS
+//
+//  Created by zupper on 02/12/19.
+//  Copyright © 2019 Luizhcarminati. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+}
